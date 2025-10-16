ead750e3c1f341e46b753513b00f9ca56668cf57
Breno Marques <brenomarques516@gmail.com>
2025-10-15 22:37:31 -0300

    Initial Commit

---
+++ PokedexUI/PokedexUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexUIApp.swift
+//  PokedexUI
+//
+//  Created by Breno Marques on 15/10/25.
+//
+
+import SwiftUI
+
+@main
+struct PokedexUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
